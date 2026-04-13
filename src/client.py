79045d2ba9e0fc07621967ea144b59f967879175
Dasha <dashazarichan@gmail.com>
2026-04-13 14:51:28 +0300

    w

--- src/client.py
+++ src/client.py
@@ -8,8 +8,11 @@ HOST = "127.0.0.1"
 PORT = 1848
 
 NICKNAME = input("Введіть ваш нікнейм:")
+client_socket = None
+
 
 def handle_server():
+    global client_socket
     client_socket = socket.socket()
     client_socket.connect((HOST, PORT))
 
@@ -17,11 +20,21 @@ def handle_server():
 
     id_data = client_socket.recv(1024).decode()
     print(f"Отримано з сервера: {id_data}")
-    
+
     while True:
         data = client_socket.recv(1024).decode()
         if not data:
             break
         print(f"Отримано з сервера: {data}")
 
-handle_server()
+
+def send_possition(dy):
+    global client_socket
+
+    if client_socket == None:
+        return
+
+    client_socket.send(json.dumps({"nickname": NICKNAME, "dy": dy}).encode())
+
+
+threading.Thread(target=handle_server).start()
--- src/server.py
+++ src/server.py
@@ -1,21 +1,24 @@
 import json
 import socket
 import threading
+import time
 
 HOST = "0.0.0.0"
 PORT = 1848
 SPEED = 10
+FPS = 1
 
 players = {}
 
+
 def handle_client(conn: socket.socket, addr):
     nickname = conn.recv(1024).decode()
-   
+
     if nickname in players:
         msg_c = conn.send("Андрій чітєр гуляй".encode())
         conn.shutdown(socket.SHUT_RDWR)
         return conn.close()
-   
+
     players[nickname] = {"y": 0, "points": 0, "socket": conn}
     msg = conn.send(f"Ваш нікнейм та ID: {nickname}, {addr}".encode())
     while True:
@@ -27,7 +30,8 @@ def handle_client(conn: socket.socket, addr):
         except:
             continue
         dy = msg.get("dy", 0)
-        players[addr]["y"] += dy * SPEED
+        players[nickname]["y"] += dy * SPEED
+        print(players)
 
     conn.close()
 
@@ -43,5 +47,20 @@ while True:
     if len(players) >= 2:
         break
 
-# thread = threading.Thread(target=process_game)
-# thread.start()
+
+def process_game():
+    print("start broadcast")
+    while True:
+        for player, data in players.items():
+            for player2, data2 in players.items():
+                data["socket"].send(
+                    json.dumps(
+                        {"type": "PLAYER_POSS", "nickname": player2, "y": data2["y"]}
+                    ).encode()
+                )
+
+        time.sleep(1 / FPS)
+
+
+thread = threading.Thread(target=process_game)
+thread.start()
